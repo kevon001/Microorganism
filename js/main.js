6b2e69e9fea46f51fe4ec6563cb953acb80b7156
okano <busbusbus21@gmail.com>
2015-10-04 20:45:51 +0900

    クマをいくつか生成して、ランダムで動くようにした

--- js/main.js
+++ js/main.js
@@ -1,39 +1,23 @@
 enchant();
 window.onload = function() {
+    var KumaNum = 3 ; //クマ数
+    var kumaList =[] ;  //クマインスタンスを保存しておくリスト
+
     var game_ = new Game(320, 320); // 表示領域の大きさを設定
     game_.fps = 24;                 // ゲームの進行スピードを設定
     game_.preload('./img/chara1.png', './img/start.png', './img/gameover.png'); // ゲームに使う素材を、あらかじめ読み込む
     game_.onload = function() { // ゲームの準備が整ったらメインの処理を実行します
         /**
-        * タイトルシーン
-        *
-        * タイトルシーンを作り、返す関数です。
-        */
+         * タイトルシーン
+         *
+         * タイトルシーンを作り、返す関数です。
+         */
         var createStartScene = function() {
             var scene = new Scene();                                // 新しいシーンを作る
             scene.backgroundColor = '#fcc800';                      // シーンの背景色を設定
             // スタート画像設定
             var startImage = new Sprite(236, 48);                   // スプライトを作る
-            startImage.image = game_.assets['./img/start.png'];     // スタート画像を設定
-            startImage.x = 42;                                      // 横位置調整
-            startImage.y = 136;                                     // 縦位置調整
             scene.addChild(startImage);                             // シーンに追加
-            // タイトルラベル設定
-            var title = new Label('くまたたき');                     // ラベルを作る
-            title.textAlign = 'center';                             // 文字を中央寄せ
-            title.color = '#ffffff';                                // 文字を白色に
-title.x = 0;                                            // 横位置調整
-            title.y = 96;                                           // 縦位置調整
-            title.font = '28px sans-serif';                         // 28pxのゴシック体にする
-            scene.addChild(title);                                  // シーンに追加
-            // サブタイトルラベル設定
-            var subTitle = new Label('- くまをたくさん叩こう  -');  // ラベルを作る
-            subTitle.textAlign = 'center';                          // 文字中央寄せ
-title.x = 0;                                            // 横位置調整
-            subTitle.y = 196;                                       // 縦位置調整
-            subTitle.font = '14px sans-serif';                      // 14pxのゴシック体にする
-            scene.addChild(subTitle);                               // シーンに追加
-            
             // スタート画像にタッチイベントを設定
             startImage.addEventListener(Event.TOUCH_START, function(e) {
                 game_.replaceScene(createGameScene());    // 現在表示しているシーンをゲームシーンに置き換える
@@ -43,110 +27,128 @@ title.x = 0;                                            // 横位置調整
         };
 
         /**
-        * ゲームシーン
-        *
-        * ゲームシーンを作り、返す関数です。
-        */
+         * ゲームシーン
+         *
+         * ゲームシーンを作り、返す関数です。
+         */
         var createGameScene = function() {
             var scene = new Scene();                            // 新しいシーンを作る
             scene.backgroundColor = '#fcc8f0';
-            
-            var time =  240;                                    // 残り時間を初期化
-            var score = 0;                                      // 得点を初期化
-            // 得点欄を作成
-            var label = new Label('スコア: ' +score+ '体叩いた！'); // スコア: ○体叩いた！と表示するラベルを作る
-            label.font = '14px sans-serif';                       // 14pxのゴシック体にする
-            scene.addChild(label);                                // シーンに追加
-            // 残り時間欄を作成
-            var timeLimit = new Label('残り時間:' + time);       // 残り時間: ○○と表示するラベルを作る
-            timeLimit.font = '14px sans-serif';                 // 14pxのゴシック体にする 
-timeLimit.x = 0;                                    // 横位置調整
-            timeLimit.y = 20;                                   // 縦位置調整
-            scene.addChild(timeLimit);                          // シーンに追加
-            // 背景テキストを作成
-            var backgroundText = new Label('くまを叩け!');
-            backgroundText.color = '#ffffff';                   // 文字を白色に
-            backgroundText.font = '60px sans-serif';            // 60pxのゴシック体にする
-            backgroundText.textAlign = 'center';                // 中央揃えにする
-            backgroundText.x = 0;                               // 横位置調整
-            backgroundText.y = 130;                             // 縦位置調整
-            scene.addChild(backgroundText);                     // シーンに追加
-            
-            // くまを作成
+
+            for(var i=0 ; i < KumaNum  ;i++){
+                kumaList.push(createKuma()) ;
+            }
+
+            for(num in kumaList){
+                scene.addChild(kumaList[num]);                               // シーンに追加
+            }
+
+            // シーンに毎フレームイベントを設定
+            scene.addEventListener(Event.ENTER_FRAME, function() {
+                moveKumas() ;   //すべてのクマを動かす
+                isLimitObjectList() ;   //すべてのクマに位置により判定を行う
+            });
+
+            // addEventListenerToKumaList(kumaList) ;
+            // くまにタッチイベントを設定する
+            return scene;
+        };
+
+        /**
+         * クマにリスナーを加える
+         * ただ、このメソッドは今は使っていない
+         */
+        function addEventListenerToKumaList(kumaList){
+            for(kuma in kumaList){
+                kuma.addEventListener(Event.TOUCH_START, function(e) {
+                    // label.text = 'スコア: ' + score + '体叩いた！';     // スコアの文言を更新
+                    kuma.x = Math.random() * 288;                       // くまの横位置を0～288pxの間にランダムで再設定(ワープ)
+                    kuma.y = Math.random() * 288;                       // くまの縦位置を0～288pxの間にランダムで再設定(ワープ)
+                    kumaSpeed = Math.random() * 8 - 4;                  // くまのスピードを-4～+4の間でランダムに再設定
+                    // くまの移動方向が左ならスプライトを反転させる
+                    if (kumaSpeed > 0) {
+                        kuma.scaleX = 1;    // 横方向の大きさを1に(通常)
+                    } else {
+                        kuma.scaleX = -1;   // 横方向の大きさをマイナス1に(反転)
+                    }
+                });
+
+            }
+
+        }
+
+        /**
+         * クマを生成
+         */
+        function createKuma(){
             var kuma = new Sprite(32, 32);                      // スプライトを作る
             kuma.image = game_.assets['./img/chara1.png'];      // くま画像を設定
             kuma.x = Math.random() * 288;                       // くまの横位置を0～288pxの間でランダムに設定
             kuma.y = Math.random() * 288;                       // くまの縦位置を0～288pxの間でランダムに設定
-            scene.addChild(kuma);                               // シーンに追加
             var kumaSpeed = Math.random() * 8 - 4;              // くまのスピードを-4～+4の間でランダムに設定
-            // くまの移動方向が左ならスプライトを反転させる
-            if (kumaSpeed > 0) {
-                kuma.scaleX = 1;    // 横方向の大きさを1に(通常)
-            } else {
-                kuma.scaleX = -1;   // 横方向の大きさをマイナス1に(反転)
-            }
-            
-            // シーンに毎フレームイベントを設定
-            scene.addEventListener(Event.ENTER_FRAME, function() {
-    
-                    kuma.x += ((Math.random() * 2) -1) * 2 ;
-                    kuma.y += ((Math.random() * 2) -1) * 2 ;
+            return kuma ;
+        }
 
-                // time --;                                // 残り時間を1ずつ減らす
-                // timeLimit.text = '残り時間:' + time;    // 残り時間の表示を更新
-                // // 時間切れ
-                // if (time <= 0) {
-                //     game_.replaceScene(createGameoverScene(score));    // 現在表示しているシーンをゲームオーバーシーンに置き換える
-                // }
-                // kuma.x += kumaSpeed;            // くまを横方向に移動
-                // // くまが画面端に来た時の対処
-                if (kuma.x > 320) {             // もしも右端に到達したら
-                    kuma.x = -32;               // 左端にワープさせる
-                } else if (kuma.x < -32) {      // もしも左端に到達したら
-                    kuma.x = 320;               // 右端にワープさせる
-                }
-
-                if (kuma.y > 320) {             // もしも右端に到達したら
-                    kuma.y = -32;               // 左端にワープさせる
-                } else if (kuma.y < -32) {      // もしも左端に到達したら
-                    kuma.y = 320;               // 右端にワープさせる
-                }
-
-                // // くまのスプライトシートのフレームを動かし、走っているような効果を与える
-                // kuma.frame ++;                  // くまのフレームを動かす
-                // if (kuma.frame > 2) {           // くまのフレームが3以上になったら
-                //     kuma.frame = 0;             // 0に戻す
-                // }
-            });
-            // くまにタッチイベントを設定する
-            kuma.addEventListener(Event.TOUCH_START, function(e) {
-                score ++;                                           // くまを叩いたらスコアに1を加える
-                label.text = 'スコア: ' + score + '体叩いた！';     // スコアの文言を更新
-                kuma.x = Math.random() * 288;                       // くまの横位置を0～288pxの間にランダムで再設定(ワープ)
-                kuma.y = Math.random() * 288;                       // くまの縦位置を0～288pxの間にランダムで再設定(ワープ)
-                kumaSpeed = Math.random() * 8 - 4;                  // くまのスピードを-4～+4の間でランダムに再設定
-                // くまの移動方向が左ならスプライトを反転させる
-                if (kumaSpeed > 0) {
-                    kuma.scaleX = 1;    // 横方向の大きさを1に(通常)
-                } else {
-                    kuma.scaleX = -1;   // 横方向の大きさをマイナス1に(反転)
-                }
-            });
-            // ゲームシーンを返す
-            return scene;
-        };
-
-     
         
+        /**
+         * すべてのクマを動かす
+         */
+        function moveKumas(){
+            for(num in kumaList) {
+                moveKuma(kumaList[num]) ;
+            }
+        }
 
         /**
-        * ゲームオーバーシーン
-        *
-        * ゲームオーバーシーンを作り、返す関数です。
-        * createGameoverScore(※引数) ※引数にスコアを入れると画面にスコアが表示されます
-        * ※は任意の名前でOKで、カンマ区切りで複数設定できます。
-        * 例) var createGameoverScore = function (resultScore, test1, test2) {
-        */
+         * クマを動かす
+         */
+        function moveKuma(kuma){
+            kuma.x += ((Math.random() * 2) -1) * 2 ;
+            kuma.y += ((Math.random() * 2) -1) * 2 ;
+
+            //以下でクマにアニメーションをつける
+            kuma.frame ++ ;
+            if(kuma.frame > 2){
+                kuma.frame = 0 ; 
+            }
+        }
+
+        /**
+         * クマの位置が画面からはみ出ないように調整
+         */
+        function isLimitObjectList(){
+            // for(int i=0 ;i < objectList.size ; i++)  {
+            for( num in kumaList) {
+                isLimit(kumaList[num])
+            }
+        }
+
+        /**
+         *　枠に収まっているかの判定し、限界であれば調整する
+         */
+        function isLimit(obj){
+            if (obj.x > 320) {             // もしも右端に到達したら
+                obj.x = -32;               // 左端にワープさせる
+            } else if (obj.x < -32) {      // もしも左端に到達したら
+                obj.x = 320;               // 右端にワープさせる
+            }
+
+            if (obj.y > 320) {             // もしも右端に到達したら
+                obj.y = -32;               // 左端にワープさせる
+            } else if (obj.y < -32) {      // もしも左端に到達したら
+                obj.y = 320;               // 右端にワープさせる
+            }
+        }
+
+
+        /**
+         * ゲームオーバーシーン
+         *
+         * ゲームオーバーシーンを作り、返す関数です。
+         * createGameoverScore(※引数) ※引数にスコアを入れると画面にスコアが表示されます
+         * ※は任意の名前でOKで、カンマ区切りで複数設定できます。
+         * 例) var createGameoverScore = function (resultScore, test1, test2) {
+         */
         var createGameoverScene = function(resultScore) {
             var scene = new Scene();                                   // 新しいシーンを作る
             scene.backgroundColor = '#303030';                         // シーンの背景色を設定
@@ -160,7 +162,7 @@ timeLimit.x = 0;                                    // 横位置調整
             var label = new Label(resultScore + '体叩いた');            // ラベルを作る スコアを代入
             label.textAlign = 'center';                                // 文字を中央寄せ
             label.color = '#fff';                                      // 文字を白色に
-label.x = 0;                                               // 横位置調整
+            label.x = 0;                                               // 横位置調整
             label.y = 60;                                              // 縦位置調整
             label.font = '40px sans-serif';                            // 40pxのゴシック体にする
             scene.addChild(label);                                     // シーンに追加
@@ -178,6 +180,6 @@ label.x = 0;                                               // 横位置調整
             return scene;
         };
         game_.replaceScene(createStartScene());  // ゲームの_rootSceneをスタートシーンに置き換える
-    }
-    game_.start(); // ゲームをスタートさせます
+}
+game_.start(); // ゲームをスタートさせます
 };
